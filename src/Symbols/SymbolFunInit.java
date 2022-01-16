b48370cbd1ff62f4bb499af1e4a5042d661acc42
Andreu Sureda <andreusureda@kuarasoftware.com>
2022-01-16 13:32:55 +0100

    FIX value symbols Fun

--- src/Symbols/SymbolFunInit.java
+++ src/Symbols/SymbolFunInit.java
@@ -3,7 +3,7 @@ package Symbols;
 public class SymbolFunInit extends SymbolBase{
 
 	public SymbolFunInit() {
-		super("Symbol Function Head", 0);
+		super("Symbol Function Initalization", 0);
 	}
 	
 }
--- src/Symbols/SymbolFunInstructions.java
+++ src/Symbols/SymbolFunInstructions.java
@@ -2,7 +2,7 @@ package Symbols;
 
 public class SymbolFunInstructions extends SymbolBase {
 
-	public SymbolFunInstructions(String variable, Integer valor) {
-		super("Symbol Function Body", 0);
+	public SymbolFunInstructions() {
+		super("Symbol Function Instructions", 0);
 	}
 }
