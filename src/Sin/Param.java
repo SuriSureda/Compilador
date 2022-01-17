672cd79e0fa8b754d291e4cf025747b29f6bcece
Andreu Sureda <andreusureda@kuarasoftware.com>
2022-01-17 01:09:49 +0100

    Arreglos de classe Param

--- src/Sin/Param.java
+++ src/Sin/Param.java
@@ -5,14 +5,20 @@ import SymbolsTable.Type;
 public class Param {
 	private String varId;
 	private Type type;
+	private int size;
 
-	public Param(String varId, Type type) {
+	public Param(String varId, Type type, int size) {
 		this.varId = varId;
 		this.type = type;
+		this.size = size;
 	}
 
 	public String getVarId() {
 		return this.varId;
+	}
+
+	public int getSize() {
+		return this.size;
 	}
 
 	public Type getType() {
