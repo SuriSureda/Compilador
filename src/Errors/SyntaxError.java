d6ecd7d0f954626f49868c99c641f5d967344b08
Antoni <apayerasmunar@gmail.com>
2022-01-29 03:40:29 -0800

    SyntaxError

--- src/Errors/SyntaxError.java
+++ src/Errors/SyntaxError.java
@@ -3,13 +3,25 @@ package Errors;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import java_cup.runtime.ComplexSymbolFactory.ComplexSymbol;
 
 public class SyntaxError extends Exception{
     private static BufferedWriter out = null;
     private final String PATH = "output\\syntaxError.txt";
 
-    public SyntaxError(String error){
-        super(error);
+    public SyntaxError(ComplexSymbol s, ArrayList<String> expectedName, boolean recovered){
+        super();
+        String error = "Syntax Error: "+ s.value+ "found in position ("+ s.left+1+":"+ s.right+1+")";
+        if (expectedName != null){
+            error += "\n Token Expected: ";
+            for (String token: expectedName){
+                error += token + " ";
+            }
+        }
+        System.out.print(error);
+
         if (out== null){
             try {
                 out = new BufferedWriter(new FileWriter(PATH));
